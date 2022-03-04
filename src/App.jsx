26fe0b9ccdae74b91aee8163ec6f9c439295bcbd
Andrew <andrew.morgan@e.org>
2022-03-04 11:42:40 +0000

    Added prelim Cart and details components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Products from "./components/products";
+import Products from "./components/Products";
 import Footer from "./Footer";
 import Header from "./Header";
 
