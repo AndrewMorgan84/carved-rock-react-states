c6d26481457a20d962f5b924a09c2d6880383255
Andrew <andrew.morgan@e.org>
2022-03-04 11:35:44 +0000

    extracted products from app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
+import Products from "./components/products";
 import Footer from "./Footer";
 import Header from "./Header";
-import { getProducts } from "./services/productService";
-import Spinner from "./Spinner";
 
 export default function App() {
-  const [size, setSize] = useState("");
-
-  function renderProduct(p) {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-        </a>
-      </div>
-    );
-  }
-
-  const filteredProducts = size
-    ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
-    : products;
-
-  if (error) throw error;
-  if (loading) return <Spinner />;
-
   return (
     <>
       <div className="content">
         <Header />
         <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select
-              id="size"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-            >
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-            {size && <h2>Found {filteredProducts.length} items</h2>}
-          </section>
-          <section id="products">{filteredProducts.map(renderProduct)}</section>
+          <Products />
         </main>
       </div>
       <Footer />
